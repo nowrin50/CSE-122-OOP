2b01075acb39ae7020cae1b4bd3c1417c84f0288
Ferdouse H. Nowrin <84663223+nowrin-ux@users.noreply.github.com>
2023-03-15 11:58:10 +0600

    Add files via upload

---
+++ lab 6/word capitalization.cpp
@@ -0,0 +1,22 @@
+
+
+//Word Capitalization
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+
+    string s;
+
+    cin >> s;
+
+    s[0] = towupper(s[0]);
+
+    cout << s;
+
+
+    return 0;
+}

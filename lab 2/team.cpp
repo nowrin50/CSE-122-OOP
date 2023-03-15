2e05b3f19059b1ca97b73aee0460f2f9a2fa17cc
Ferdouse H. Nowrin <84663223+nowrin-ux@users.noreply.github.com>
2023-03-15 08:44:35 +0600

    Add files via upload

---
+++ lab 2/team.cpp
@@ -0,0 +1,20 @@
+
+
+//Problem Name-- TEAM
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count = 0, p, v, t, n;
+    cin>>n;
+    while(n){
+        cin>>p>>v>>t;
+        if(p+v+t>=2){
+            count++;
+        }
+        n--;
+    }
+    cout<<count<<endl;
+    return 0;
+}

57f1b8778bd2f26436fd65cb0d7708a92bc44494
Ferdouse H. Nowrin <84663223+nowrin-ux@users.noreply.github.com>
2023-03-15 08:11:32 +0600

    Add files via upload

---
+++ lab 1/lab1.cpp
@@ -0,0 +1,23 @@
+
+// Watermelon
+
+
+#include<stdio.h>
+
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    if(n <= 2 || n%2 != 0){
+    printf("NO\n");
+    }
+
+    else{
+
+    printf("YES\n");
+    }
+
+    return 0;
+}

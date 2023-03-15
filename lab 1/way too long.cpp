10b768c453da9987cc8f01a0ef5fc1dc315aba9b
Ferdouse H. Nowrin <84663223+nowrin-ux@users.noreply.github.com>
2023-03-15 08:41:47 +0600

    Add files via upload

---
+++ lab 1/way too long.cpp
@@ -0,0 +1,35 @@
+
+
+//lab 2
+//problem 71A
+//Problem name-- Way too long
+
+
+
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+
+using namespace std;
+int main()
+
+{
+
+    int n,l;
+    cin>>n;
+    string s;
+    while(n--)
+    {
+     cin>>s;
+    l=s.length();
+    if(l<=10)
+    {
+        cout<<s<<endl;
+    }
+    else
+        {
+        cout<<s[0]<<l-2<<s[l-1]<<endl;
+    }
+}
+return 0;
+}
